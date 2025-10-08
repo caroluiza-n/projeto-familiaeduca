594ffd8d16dcdac0c92c12729f03f3cfb2feee3f
Caroline <caroluizanunes126@gmail.com>
2025-10-08 16:09:47 -0300

    criado docker compose para database

---
+++ src/main/java/com/projeto/familiaeduca/FamiliaeducaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.familiaeduca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliaeducaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamiliaeducaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/familiaeduca/FamiliaeducaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.familiaeduca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamiliaeducaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
