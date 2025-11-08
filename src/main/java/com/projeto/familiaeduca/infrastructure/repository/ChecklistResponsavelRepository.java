85436e7c16e138781c7d6827ddf04062e5a003e1
Caroline <caroluizanunes126@gmail.com>
2025-11-08 14:11:40 -0300

    adicionando imports

--- src/main/java/com/projeto/familiaeduca/infrastructure/repository/ChecklistResponsavelRepository.java
+++ src/main/java/com/projeto/familiaeduca/infrastructure/repository/ChecklistResponsavelRepository.java
@@ -3,7 +3,7 @@ package com.projeto.familiaeduca.infrastructure.repository;
 import com.projeto.familiaeduca.domain.models.ChecklistResponsavel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import java.util.UUID;
 
 @Repository
